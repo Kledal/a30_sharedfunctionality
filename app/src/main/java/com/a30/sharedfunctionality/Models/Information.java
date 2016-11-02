b39f0265fa9bc5e051bb30ac69bed1e64f8753d3
Alexander Kledal <201371052@iha.dk>
2016-11-02 18:42:01 +0100

    Add key to information

--- app/src/main/java/com/a30/sharedfunctionality/Models/Information.java
+++ app/src/main/java/com/a30/sharedfunctionality/Models/Information.java
@@ -4,6 +4,7 @@ import com.google.firebase.database.IgnoreExtraProperties;
 
 @IgnoreExtraProperties
 public class Information {
+    public String key;
     public String title;
     public String description;
     public String link;
