04fd42736c1f3d6a2bd4b73564d90fb3aba51ab5
Alexander Kledal <201371052@iha.dk>
2016-11-02 17:19:18 +0100

    Add information model

---
+++ app/src/main/java/com/a30/sharedfunctionality/Models/Information.java
@@ -0,0 +1,10 @@
+package com.a30.sharedfunctionality.Models;
+
+import com.google.firebase.database.IgnoreExtraProperties;
+
+@IgnoreExtraProperties
+public class Information {
+    public String title;
+    public String description;
+    public String link;
+}
