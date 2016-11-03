0243bc2a1615c4763182889131db1979987e84b7
Alexander Kledal <201371052@iha.dk>
2016-11-03 19:36:45 +0100

    Add perform independently

--- app/src/main/java/com/a30/sharedfunctionality/Models/SchemeRow.java
+++ app/src/main/java/com/a30/sharedfunctionality/Models/SchemeRow.java
@@ -3,9 +3,12 @@ package com.a30.sharedfunctionality.Models;
 public class SchemeRow {
     public String name;
     public long date;
+
+    public String caretaker;
     public String lectured;
     public String guidance;
-    public String caretaker;
+    public String performIndependently;
+
 
     public SchemeRow() { }
 }
