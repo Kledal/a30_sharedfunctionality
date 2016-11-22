75543e05724f51c992a1c5a5608c749348584c87
Alexander Kledal <201371052@iha.dk>
2016-11-22 17:20:59 +0100

    add missing public

--- app/src/main/java/com/a30/sharedfunctionality/Models/DailyRhythm.java
+++ app/src/main/java/com/a30/sharedfunctionality/Models/DailyRhythm.java
@@ -3,7 +3,7 @@ package com.a30.sharedfunctionality.Models;
 import java.util.Calendar;
 
 public class DailyRhythm extends BaseSchema {
-    String dailyRhythm;
+    public String dailyRhythm;
 
     public DailyRhythm() {
         createdAt = Calendar.getInstance().getTimeInMillis();
